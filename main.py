b308ece1fb660e47fea348aaeb3d41dcbe70e992
Kei-KC <kyc355@nyu.edu>
2022-10-01 15:48:46 -0400

    Update main.py

--- main.py
+++ main.py
@@ -7,5 +7,21 @@ def main():
 		""")
 
 	cmd = input("Enter a command: ")
+	while(cmd != "exit"): 
 
+		# CMD : VIEWALL
+		if (cmd == 'viewAll' || cmd == 'viewall' || cmd == 1):
+			# TODO :
+			#	pull all recipes from JSON file and loop over recipe names
+			#	print recipe names in terminal
+			#	prompt user to select a recipe
+
+		# CMD : SEARCH
+		else if (cmd == 'search' || cmd == 2):
+			# TODO:
+			#	prompt user for a recipe name 
+			#	maybe implement fuzzy search
+
+		cmd = input("Enter a command: ")
+	exit();
 main()
