003d7f6e2e0a32d4bd028eef4adb05bf659c3697
andrewCoder2024 <67741322+andrewCoder2024@users.noreply.github.com>
2022-10-02 11:27:16 -0400

    added basic ppandas functionality for database

---
+++ json_db.py
@@ -0,0 +1,16 @@
+import json
+import pandas 
+
+def recipe_creation(name, ingredients, instructions):
+    df = pd.read_csv('recipe_book.csv')
+    if name in df['name'].values:
+        return
+    recipe = {'name':name,'ingredients':ingredients,'instructions':instructions}
+    df.append(recipe)
+    df.to_csv('recipe_book.csv')
+
+def recipe_search(search_term):
+    df = pd.read_csv('recipe_book.csv')
+    if search_term in df['name'].values:
+        return df.query('name' == search_term)
+
