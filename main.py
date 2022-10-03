fcec2f125ce6077a4b568eeb84848fa6e8051688
Kei-KC <kyc355@nyu.edu>
2022-10-03 16:08:16 -0400

    add `print_whole()` and `step_by_step()` helper functions

--- main.py
+++ main.py
@@ -1,20 +1,49 @@
-def main():
-	print("""Welcome To Pythonees Recipe Book.
-		Current functionalities of the recipe book include:
-		1) View All Recipes (cmd: viewAll)
-		2) Seach For Recipe (cmd: search)
-		3) Exit (cmd: exit)
-		""")
+import json
+import json_db as db
+import os # for clearing the terminal
 
-	cmd = input("Enter a command: ")
-	while(cmd != "exit"): 
+
+# PRINT ENTIRE RECIPE 
+def print_whole(recipe):
+	name = recipe["name"]
+	ingredients_arr = recipe["ingredients"].str.split(",")
+	instructions_arr = recipe["instructions"].str.split(";")
+	print("{} Recipe\n".format(name))
+
+	# print ingredients
+	print("Ingredients:\n")
+	for ing in ingredients_arr: 
+		print("{}\n".format(ing))
+
+	# print instructions
+	print("Instructions:\n")
+	for ins in instructions_arr: 
+		print("{}\n".format(ins))
+
+	return
+
+# PRINT RECIPE STEP-BY-STEP
+def step_by_step(recipe):
+	return
+
+def main():
+	cmd = ""
+	while(cmd != "exit"):
+		print("""Welcome To Pythonees Recipe Book.
+			Current functionalities of the recipe book include:
+			1) View All Recipes (cmd: viewAll)
+			2) Seach For Recipe (cmd: search)
+			3) Exit (cmd: exit)
+			""")
+		cmd = input("Enter a command: ")
 
 		# CMD : VIEWALL
-		if (cmd == 'viewAll' || cmd == 'viewall' || cmd == 1):
+		if (cmd == 'viewAll' or cmd == 'viewall' or cmd == 1):
 			# TODO :
 			#	pull all recipes from JSON file and loop over recipe names
 			#	print recipe names in terminal
 			#	prompt user to select a recipe
+			return
 
 		# CMD : SEARCH
 		else if (cmd == 'search' || cmd == 2):
@@ -24,4 +53,6 @@ def main():
 
 		cmd = input("Enter a command: ")
 	exit();
+
 main()
+f.close()
