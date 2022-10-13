efb0721fde1a2b75d88b69e06d2b0e42dd81f9ab
Ruby Kim <ruby@10-16-78-211.dynapool.wireless.nyu.edu>
2022-10-13 12:49:09 -0400

    updated features

---
+++ app_create_p1.py
@@ -0,0 +1,31 @@
+import tkinter as tk
+from tkinter import *
+import os
+import print_db
+import subprocess as sub
+
+
+# BASIC COMPONENTS
+c_root = tk.Tk()
+c_root.title('Pythonees Recipe Book')
+c_root.resizable(False, False) # WINDOW NOT RESIZABLE
+canvas = tk.Canvas(c_root, height = "700", width = "700", bg = "#e3e398")
+canvas.pack()
+frame = tk.Frame(c_root, bg = "#FFF")
+frame.place(relheight = 0.7, relwidth = 0.7, relx = 0.5, rely = 0.5, anchor = CENTER) 
+
+msg = tk.Label(frame, text = "Create your own!",
+                fg = "#000", bg = "#e3e398", 
+                font = ("Consolas 15", 20))
+msg.place(anchor = CENTER, relx = 0.5, rely = 0.1)
+
+# BACK TO START
+def prev(): 
+    c_root.destroy()
+    import app
+    app.root.deiconify()
+     
+prev = tk.Button(frame, text = "Previous", padx = 5, pady = 5, 
+                    fg="#000", bg="gray", font=('Consolas 15', 14),
+                    command = prev)
+prev.place(anchor = CENTER, relx= 0.5, rely=0.8)
