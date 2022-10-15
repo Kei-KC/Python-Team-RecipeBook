ae5a49d5d981c5418fbe36c19f6516b0cfe50255
Ruby Kim <ruby@10-20-6-82.dynapool.wireless.nyu.edu>
2022-10-14 23:52:12 -0400

    print all menus (cmd output) to GUI

--- app_viewall_p1.py
+++ app_viewall_p1.py
@@ -2,31 +2,57 @@ import tkinter as tk
 from tkinter import *
 import os
 import print_db
-import subprocess as sub
 from tkmacosx import Button as button
 
 # BASIC COMPONENTS
-v_root = tk.Tk()
-v_root.title('Pythonees Recipe Book')
-v_root.resizable(False, False) # WINDOW NOT RESIZABLE
-canvas = tk.Canvas(v_root, height = "700", width = "700", bg = "#e3e398")
-canvas.pack()
-frame = tk.Frame(v_root, bg = "#FFF")
-frame.place(relheight = 0.7, relwidth = 0.7, relx = 0.5, rely = 0.5, anchor = CENTER) 
+def view(data):
+    v_root = tk.Tk()
+    v_root.title('Pythonees Recipe Book')
+    v_root.resizable(False, False) # WINDOW NOT RESIZABLE
+    canvas = tk.Canvas(v_root, height = "700", width = "700", bg = "#e3e398")
+    canvas.pack()
+    frame = tk.Frame(v_root, bg = "#FFF")
+    frame.place(relheight = 0.7, relwidth = 0.7, relx = 0.5, rely = 0.5, anchor = CENTER) 
 
-msg = tk.Label(frame, text = "Pythonees Recipe Book Menu",
-                fg = "#000", bg = "#e3e398", 
-                font = ("Consolas 15", 25, 'bold'))
-msg.place(anchor = CENTER, relx = 0.5, rely = 0.1)
+    menu_msg = tk.Label(frame, text = "Pythonees Menu",
+                    fg = "#000", bg = "#e3e398", 
+                    font = ("Consolas 15", 25, 'bold'))
+    menu_msg.place(anchor = CENTER, relx = 0.5, rely = 0.1)
+    select_msg = tk.Label(frame, text = "Select a menu to view recipe!",
+                    fg = "#000", bg = "#e3e398", 
+                    font = ("Consolas 15", 25, 'bold'))
+    select_msg.place(anchor = CENTER, relx = 0.5, rely = 0.1)
 
-# BACK TO START
-def prev(): 
-    v_root.destroy()
-    import app
-    app.root.deiconify()
-     
-    
-prev = button(canvas, text = "Previous", padx = 5, pady = 5, bd = 0,
-                    fg="#000", bg="#fff", font=('Consolas 15', 14), 
-                    command = prev)
-prev.place(anchor = CENTER, relx= 0.5, rely=0.9)
+    # PRINT ALL MENU
+    menu_list = list(data.values()) # LIST OF MENUS
+    menu1 = tk.Label(frame, text = menu_list[0].capitalize(), 
+                     bg = "#fff", fg = "#000",
+                     font = ("Consolas 15", 25, 'bold'))
+    menu1.place(anchor = CENTER, relx = 0.5, rely = 0.3)
+
+    menu2 = tk.Label(frame, text = menu_list[1].capitalize(), 
+                     bg = "#fff", fg = "#000",
+                     font = ("Consolas 15", 25, 'bold'))
+    menu2.place(anchor = CENTER, relx = 0.5, rely = 0.4)
+
+    menu3 = tk.Label(frame, text = menu_list[2].capitalize(), 
+                     bg = "#fff", fg = "#000",
+                     font = ("Consolas 15", 25, 'bold'))
+    menu3.place(anchor = CENTER, relx = 0.5, rely = 0.5)
+
+    menu4 = tk.Label(frame, text = menu_list[3].capitalize(), 
+                     bg = "#fff", fg = "#000",
+                     font = ("Consolas 15", 25, 'bold'))
+    menu4.place(anchor = CENTER, relx = 0.5, rely = 0.6)
+
+    # BACK TO START
+    def prev(): 
+        v_root.destroy()
+        import app
+        app.root.deiconify()
+        
+        
+    prev = button(canvas, text = "Previous", padx = 5, pady = 5, borderless = 1,
+                        fg="#000", bg="#fff", font=('Consolas 15', 14),
+                        command = prev)
+    prev.place(anchor = CENTER, relx= 0.5, rely=0.9)
