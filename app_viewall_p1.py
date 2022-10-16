15e833d7f199d7b158f657c97376c97c78211e1a
Ruby Kim <ruby@10-20-6-82.dynapool.wireless.nyu.edu>
2022-10-15 21:50:47 -0400

    updated

--- app_viewall_p1.py
+++ app_viewall_p1.py
@@ -1,13 +1,11 @@
+import pandas as pd
 import tkinter as tk
 from tkinter import *
-import os
-import print_db
 from tkmacosx import Button as button
+import app_show_recipe as show
 
 # NEW WINDOW
-
-
-def view(data):
+def view():
     v_root = tk.Tk()
     v_root.title('Pythonees Recipe Book')
     v_root.resizable(False, False)  # window not resizable
@@ -19,80 +17,48 @@ def view(data):
     menu_msg = tk.Label(canvas, text="Pythonees Menu", bg="#e3e398",
                         fg="#000", font=("Consolas 15", 25, 'bold'))
     menu_msg.place(anchor=CENTER, relx=0.5, rely=0.1)
-    select_msg = tk.Label(frame, text="Select one to view the recipe!", bg="#fff",
+    select_msg = tk.Label(frame, text="Select to view the recipe!", bg="#fff",
                           fg="#000", font=("Consolas 15", 20, 'bold'))
     select_msg.place(anchor=CENTER, relx=0.5, rely=0.1)
 
-    # SHOW RECIPE
-    # def show_recipe(recipe):
-    #     r_root = tk.Tk()
-    #     r_root.title('Pythonees Recipe Book')
-    #     r_root.resizable(False, False)
-    #     canvas = tk.Canvas(r_root, height="700", width="700", bg="#e3e398")
-    #     canvas.pack()
-    #     frame = tk.Frame(r_root, bg="#FFF")
-    #     frame.place(relheight=0.7, relwidth=0.7,
-    #                 relx=0.5, rely=0.5, anchor=CENTER)
+    def get_menus():
+        df = pd.read_csv('recipe_book.csv')
+        data = {}
+        for idx, recipes in df.iterrows():
+            name = recipes['name']
+            data[idx+1] = name
+        menu_list = list(data.values())
+        return menu_list
 
-    #     names, ingredients, instructions = print_db.get_recipe(recipe)
-
-    #     # print recipe name
-    #     for key in names:
-    #         recipe_name = key.capitalize() + " Recipe"
-    #     name = tk.Label(frame, text=recipe_name, fg="#000", bg="#e3e398",
-    #                     font=("Consolas 15", 25, 'bold'))
-    #     name.place(anchor=CENTER, relx=0.5, rely=0.1)
-
-    #     # print ingredients
-    #     i = 1
-    #     ingredient_all = ""
-    #     while i <= len(ingredients):
-    #         for key in ingredients.keys():
-    #             ingredient_all += str(i) + ". " + key + "\n"
-    #             i += 1
-    #     ingredient = tk.Label(frame, text="[ Ingredients ]\n" + ingredient_all,
-    #                           fg="#000", bg="#fff", font=("Consolas 15", 21, 'bold'))
-    #     ingredient.place(anchor=CENTER, relx=0.5, rely=0.35)
-
-    #     # print instructions
-    #     i = 1
-    #     instruction_all = ""
-    #     while i <= len(instructions):
-    #         for key in instructions.keys():
-    #             instruction_all += key + "\n"
-    #             i += 1
-    #     instruction = tk.Label(frame, text="[ Instructions ]\n" + instruction_all,
-    #                            fg="#000", bg="#fff", font=("Consolas 15", 21, 'bold'))
-    #     instruction.place(anchor=CENTER, relx=0.5, rely=0.6)
-
-    def test():
-        print("test")
     # PRINT ALL MENU
-    menu_list = list(data.values())  # list of menus
+    menu_list = get_menus()
     m_bg = "#fff"
     m_font_color = "#000"
     m_font_style = ("Consolas 15", 25, 'bold')
-    menu1 = tk.Label(frame, text=menu_list[0].capitalize(),
-                     bg=m_bg, fg=m_font_color, font=m_font_style)
+
+    menu1 = button(frame, text=menu_list[0].capitalize(), borderless=1,
+                     bg=m_bg, fg=m_font_color, font=m_font_style,
+                     command=show.show_recipe(menu_list[0])
+                     )
     menu1.place(anchor=CENTER, relx=0.5, rely=0.3)
-    from app_show_recipe import show_recipe
-    menu1.bind("<ButtonRelease>", show_recipe(menu_list[0]))
-    # menu1.bind("<Button>", test())
 
-    menu2 = tk.Label(frame, text=menu_list[1].capitalize(),
-                     bg=m_bg, fg=m_font_color, font=m_font_style)
+    menu2 = button(frame, text=menu_list[1].capitalize(), borderless=1,
+                     bg=m_bg, fg=m_font_color, font=m_font_style,
+                     command=show.show_recipe(menu_list[1])
+                     )
     menu2.place(anchor=CENTER, relx=0.5, rely=0.4)
-    menu2.bind("<ButtonRelease>", show_recipe(menu_list[1]))
 
-    menu3 = tk.Label(frame, text=menu_list[2].capitalize(),
-                     bg=m_bg, fg=m_font_color, font=m_font_style)
+    menu3 = button(frame, text=menu_list[2].capitalize(), borderless=1,
+                     bg=m_bg, fg=m_font_color, font=m_font_style,
+                     command=show.show_recipe(menu_list[2])
+                     )
     menu3.place(anchor=CENTER, relx=0.5, rely=0.5)
-    menu3.bind("<ButtonRelease>", show_recipe(menu_list[2]))
 
-    menu4 = tk.Label(frame, text=menu_list[3].capitalize(),
-                     bg=m_bg, fg=m_font_color, font=m_font_style)
+    menu4 = button(frame, text=menu_list[3].capitalize(), borderless=1,
+                     bg=m_bg, fg=m_font_color, font=m_font_style,
+                     command=show.show_recipe(menu_list[3])
+                     )
     menu4.place(anchor=CENTER, relx=0.5, rely=0.6)
-    menu4.bind("<ButtonRelease>", show_recipe(menu_list[3]))
 
     # BACK TO START
     def prev():
